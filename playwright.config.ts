21ac8be8f3ca54a26f9473b91efa341eecddfe13
Aviran Aviv <aviranaviv@gmail.com>
2024-09-26 02:11:19 +0300

    fix docs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,39 +1,19 @@
-import {defineConfig, devices} from '@playwright/test';
+import {defineConfig} from '@playwright/test';
+import {devices} from 'playwright';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
     testDir: './tests',
-    /* Run tests in files in parallel */
     fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [['list', { printSteps: true }]],
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'https://drpt-external-dev.myshopify.com/',
-        viewport: null,
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry'
+        viewport: null
     },
 
-    /* Configure projects for major browsers */
     projects: [
         {
             name: 'Google Chrome',
-            use: {
-                channel: 'chrome',
-                launchOptions: {
-                    args: ['--start-maximized']
-                }
-            }
+            use: { ...devices['Desktop Chrome'], channel: 'chrome', viewport: { width: 1920, height: 1080 } }
         }
     ]
 });
